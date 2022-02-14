17e83df5d594103f12e56c870a32a26d0d9351ae
scanessa <94068439+scanessa@users.noreply.github.com>
2022-02-14 17:59:57 +0100

    Included page_dewarp call in OCR, global thresholding before page_dewarp
    Signed-off-by: scanessa <94068439+scanessa@users.noreply.github.com>

--- OCR.py
+++ OCR.py
@@ -1,6 +1,5 @@
-import cv2, pytesseract, os, glob
+import cv2, pytesseract, os, glob, subprocess
 from pdf2image import convert_from_path
-import numpy as np
 
 os.chdir('P:/2020/14/Kodning/Scans')
 
@@ -28,34 +27,33 @@ def pdf_to_jpg(pdf):
 def preprocess(img_path):
     image = cv2.imread(img_path)
     gray1 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.bitwise_not(gray1)
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    thresh = cv2.threshold(gray1, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
     return thresh
     
-
 def jpg_to_string(path):
     string_list = []
     for image in path:
-        img = cv2.imread(image)
+        #Apply global thresholding only to scanned docs, take out for photos
+        cv2.imwrite(image.split('.')[0] + '_thresh.jpg', preprocess(image))
+        
+        subprocess.call([
+            'python', 
+            'P:/2020/14/Kodning/Code/page_dewrap/page_dewarp.py', 
+            image.split('.')[0] + '_thresh.jpg'
+            ])
+        
+        img = cv2.imread(image.split('.')[0] + '_thresh_straight.png')
         custom_config = r'--oem 3 --psm 6'
         img_string = pytesseract.image_to_string(img, config=custom_config, lang='swe')
         string_list.append(img_string) 
     return string_list
 
-def display(_original):
-    print(_original)
-    og = cv2.imread(_original)
-    cv2.imwrite('P:/2020/14/Kodning/Scans/original.jpg', og)
-    cv2.imwrite('P:/2020/14/Kodning/Scans/new.jpg', preprocess(_original))
+#display('sample1_pg2.jpg')
 
-#display(r"P:/2020/14/Kodning/Scans/sample1.jpg")
-
-full_text = jpg_to_string(['P:/2020/14/Kodning/Scans/sample1_pg1.jpg'])
 #Execute
-"""
+
 for pdf_file in pdf_files:
     print('Currently reading with OCR: ', pdf_file)
     jpg_paths = pdf_to_jpg(pdf_file)
     full_text = jpg_to_string(jpg_paths)
-"""
-print(full_text)
+    print(full_text)
