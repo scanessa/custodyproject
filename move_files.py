22a4c9df6b4bbb8a2e3b8aafd633ed1b6e0b9376
scanessa <94068439+scanessa@users.noreply.github.com>
2022-02-17 14:34:59 +0100

    Added renaming function
    Signed-off-by: scanessa <94068439+scanessa@users.noreply.github.com>

--- move_files.py
+++ move_files.py
@@ -6,9 +6,12 @@ Created on Wed Feb  2 14:49:04 2022
 """
 import shutil, pathlib, os.path
 
-pdf_dir = 'P:/2020/14/Kodning/Scans/Sample'
+#pdf_dir = "P:/2020/14/Tingsrätter/"
 filepaths_doc = 'P:/2020/14/Kodning/Clean_csvs_move_files/filepaths.txt' 
+includes = 'Test'
+
 noCopy = 0 
+i = 0
 error_paths = []  
 
 def read_filepaths(doc):
@@ -34,9 +37,22 @@ def true_file(files):
         if not os.path.isfile(fname):
             print('N: ', fname)
             
+def rename_file(rootdir):
+    i = 0
+    for subdir, dirs, files in os.walk(rootdir, topdown=True):
+        court = subdir.split('/')[4]
+        for file in files:
+            counter = str(i)
+            if includes in subdir and file.endswith('.pdf'):
+                file_new = court + counter + '.pdf'
+                os.rename(os.path.join(rootdir, file), os.path.join(rootdir, file_new))
+                i += 1
+                continue
+
 #Execute
-sample = read_filepaths(filepaths_doc)
-copy_files(sample, noCopy, error_paths)
+#sample = read_filepaths(filepaths_doc)
+#copy_files(sample, noCopy, error_paths)
+#rename_file(pdf_dir)
 
 #Output
 print('%d files threw an error: ' %(noCopy), error_paths)
