2567926f3f2e195bad10e6649ef355c4d2b174da
scanessa <94068439+scanessa@users.noreply.github.com>
2022-03-03 08:38:07 +0100

    Clean up git
    Signed-off-by: scanessa <94068439+scanessa@users.noreply.github.com>

--- testingcode.py
+++ testingcode.py
@@ -1,2 +1,2 @@
 from OCR import main
-firstpage_form, lastpage_form, fulltext_form, judge_string, header = main()
+firstpage_form, lastpage_form, fulltext_form, judge_string, header = main('P:/2020/14/Kodning/Scans/Stockholm3.pdf')
