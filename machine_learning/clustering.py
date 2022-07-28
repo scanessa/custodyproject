4ad5b05ef0818520f68d17c76b17780a1cf1a88d
scanessa <94068439+scanessa@users.noreply.github.com>
2022-07-28 07:51:45 +0200

    Experimented with comparing signatures with no good result
    Signed-off-by: scanessa <94068439+scanessa@users.noreply.github.com>

--- machine_learning/clustering.py
+++ machine_learning/clustering.py
@@ -1,18 +1,11 @@
 """
-Clustering images into similar groups
-Taken from: https://towardsdatascience.com/how-to-cluster-images-based-on-visual-similarity-cd6e7209fe34
-Author: Gabe Flomo
-
+Clustering images into similar groups based on autoencoders and k-means
+Sources:
+Simple k-means: https://towardsdatascience.com/how-to-cluster-images-based-on-visual-similarity-cd6e7209fe34
 Determine k: https://www.geeksforgeeks.org/ml-determine-the-optimal-value-of-k-in-k-means-clustering/#:%7E:text=There%20is%20a%20popular%20method,fewer%20elements%20in%20the%20cluster
 
-Instructions:
-    - Move all signature files to path folder
-    - Run code only until DETERMINE K: Determine number of clusters (K); number of clusters should be most notable dip in plot
-    - Once determined K, run full code to get clusters
+Autoencoder: https://minimatech.org/autoencoder-with-manifold-learning-for-clustering-in-python/
     
-TO DO:
-    Try different clustering methods (see options here: https://scikit-learn.org/stable/auto_examples/cluster/plot_cluster_comparison.html)
-    Guessing the K for k-means in a sample like this, where the groups are not very nicely seperated, is quite imprecise
 """
 
 # for loading/processing the images  
@@ -26,23 +19,35 @@ from keras.models import Model
 # clustering and dimension reduction
 from sklearn.decomposition import PCA
 from scipy.spatial.distance import cdist
+from sklearn.cluster import KMeans
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+from keras.models import Model
+from keras.layers import Dense, Input
+from keras.preprocessing import sequence
+from sklearn.model_selection import train_test_split
+from sklearn.cluster import DBSCAN
+from sklearn.manifold import Isomap
 
 # for everything else
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
-from sklearn.cluster import KMeans
+import umap.umap_ as umap
+import seaborn as sns
 
-PATH = r"P:/2020/14/Kodning/Scans/all_scans/100signatures/"
+
+PATH = r"P:/2020/14/Kodning/Scans/all_scans/100signatures/signatures/"
 FILE_ENDING = '.jpg'
+data = {}
 
 path = PATH
 # change the working directory to the path where the images are located
 os.chdir(path)
 
 # this list holds all the image filename
-flowers = []
+signs = []
 
 # creates a ScandirIterator aliased as files
 with os.scandir(path) as files:
@@ -50,7 +55,7 @@ with os.scandir(path) as files:
     for file in files:
         if file.name.endswith(FILE_ENDING):
           # adds only the image files to the flowers list
-            flowers.append(file.name)
+            signs.append(file.name)
 
 model = VGG16()
 model = Model(inputs = model.inputs, outputs = model.layers[-2].output)
@@ -68,18 +73,15 @@ def extract_features(file, model):
     features = model.predict(imgx, use_multiprocessing=True)
     return features
    
-data = {}
-p = PATH
-
 # lop through each image in the dataset
-for flower in flowers:
+for sign in signs:
     # try to extract the features and update the dictionary
     try:
-        feat = extract_features(flower,model)
-        data[flower] = feat
+        feat = extract_features(sign,model)
+        data[sign] = feat
     # if something fails, save the extracted features as a pickle file (optional)
     except:
-        with open(p,'wb') as file:
+        with open(PATH,'wb') as file:
             pickle.dump(data,file)
           
  
@@ -97,44 +99,162 @@ pca = PCA(n_components=100, random_state=22)
 pca.fit(feat)
 X = pca.transform(feat)
 
-"""
-DETERMINE K
-"""
+X_train, X_rem, y_train, y_rem = train_test_split(X, X, train_size=0.8)
+X_valid, X_test, y_valid, y_test = train_test_split(X_rem,y_rem, test_size=0.5)
 
-inertia = []
-distortions =  []
-mapping1 = {}
-mapping2 = {}
+max_len = 500
+X_train = sequence.pad_sequences(X_train, maxlen = max_len)
+X_test = sequence.pad_sequences(X_test, maxlen = max_len)
 
-list_k = list(range(3, 50))
-
-for k in list_k:
-    km = KMeans(n_clusters=k, random_state=22)
-    km.fit(X)
-    distortions.append(sum(np.min(cdist(X, km.cluster_centers_,
-                                        'euclidean'), axis=1)) / X.shape[0])
-    inertia.append(km.inertia_)
-    
-    mapping1[k] = sum(np.min(cdist(X, km.cluster_centers_,
-                                   'euclidean'), axis=1)) / X.shape[0]
-    mapping2[k] = km.inertia_
-
-# Plot sse against k
-plt.figure(figsize=(6, 6))
-plt.plot(list_k, inertia)
-plt.xlabel(r'Number of clusters *k*')
-plt.ylabel('Sum of squared distance');
-
-print("Mapping 2:\n")
-for key, val in mapping2.items():
-	print(f'{key} : {val}')
+## define the encoder
+inputs_dim = X_train.shape[1]
+encoder = Input(shape = (inputs_dim, ))
+e = Dense(1024, activation = "relu")(encoder)
+e = Dense(512, activation = "relu")(e)
+e = Dense(256, activation = "relu")(e)
 
 
-SELECTED_K = 32
+## bottleneck layer
+n_bottleneck = 10
+## defining it with a name to extract it later
+bottleneck_layer = "bottleneck_layer"
+# can also be defined with an activation function, relu for instance
+bottleneck = Dense(n_bottleneck, name = bottleneck_layer)(e)
+
+## define the decoder (in reverse)
+decoder = Dense(256, activation = "relu")(bottleneck)
+decoder = Dense(512, activation = "relu")(decoder)
+decoder = Dense(1024, activation = "relu")(decoder)
+
+
+## output layer
+output = Dense(inputs_dim)(decoder)
+## model
+model = Model(inputs = encoder, outputs = output)
+model.summary()
+
+# Model: "model"
+# _________________________________________________________________
+# Layer (type) Output Shape Param #
+# =================================================================
+# input_2 (InputLayer) [(None, 500)] 0
+# _________________________________________________________________
+# dense_14 (Dense) (None, 1024) 513024
+# _________________________________________________________________
+# dense_15 (Dense) (None, 512) 524800
+# _________________________________________________________________
+# dense_16 (Dense) (None, 256) 131328
+# _________________________________________________________________
+# bottleneck_layer (Dense) (None, 10) 2570
+# _________________________________________________________________
+# dense_17 (Dense) (None, 256) 2816
+# _________________________________________________________________
+# dense_18 (Dense) (None, 512) 131584
+# _________________________________________________________________
+# dense_19 (Dense) (None, 1024) 525312
+# _________________________________________________________________
+# dense_20 (Dense) (None, 500) 512500
+# =================================================================
+# Total params: 2,343,934
+# Trainable params: 2,343,934
+# Non-trainable params: 0
+# _________________________________________________________________
+
+
+## extracting the bottleneck layer we are interested in the most
+## in case you haven't defined it as a layer on it own you can extract it by name 
+#bottleneck_encoded_layer = model.get_layer(name = bottleneck_layer).output
+## the model to be used after training the autoencoder to refine the data
+#encoder = Model(inputs = model.input, outputs = bottleneck_encoded_layer)
+# in case you defined it as a layer as we did
+encoder = Model(inputs = model.input, outputs = bottleneck)
+
+model.compile(loss = "mse", optimizer = "adam")
+
+history = model.fit(
+    X_train,
+    X_train,
+    batch_size = 32,
+    epochs = 25,
+    verbose = 1,
+    validation_data = (X_test, X_test)
+)
+
+# Epoch 1/25
+# 782/782 [==============================] - 8s 9ms/step - loss: 0.0232 - val_loss: 0.0205
+# ...
+# ...
+# Epoch 25/25
+# 782/782 [==============================] - 6s 8ms/step - loss: 0.0152 - val_loss: 0.0176
+
+history_dict = history.history
+loss_values = history_dict["loss"]
+val_loss_values = history_dict["val_loss"]
+epochs = range(25)
+plt.plot(epochs, loss_values, "bo", label = "training loss")
+plt.plot(epochs, val_loss_values, "b", label = "validation loss")
+plt.title("Training & validation loss")
+plt.xlabel("epoch")
+plt.ylabel("loss")
+plt.legend()
+plt.show()
+# plt.clf() # to clear
+
+## representing the data in lower dimensional representation or embedding
+review_encoded = encoder.predict(X_train)
+review_encoded.shape
+# (25000, 10)
+
+## install umap library use
+# pip install umap-learn
+
+review_umapped = umap.UMAP(n_components = n_bottleneck / 2, 
+                           metric = "euclidean",
+                           n_neighbors = 50, 
+                           min_dist = 0.0,
+                           random_state = 13).fit_transform(review_encoded)
+review_umapped.shape
+# (25000, 5)
+
+np.random.seed(13)
+
+review_isomapped = Isomap(n_components = n_bottleneck / 2,
+                          n_neighbors = 50,
+                          metric = "euclidean").fit_transform(review_encoded)
+
+np.random.seed(13)
+
+clusters = DBSCAN(
+    min_samples = 50,
+    eps = 1
+).fit_predict(review_umapped)
+len(set(clusters))
+# 5
+
+
+tsne2 = TSNE(2, metric = "euclidean", random_state = 13).fit_transform(review_encoded)
+
+sns.scatterplot(tsne2[:, 0], tsne2[:, 1], 
+                hue = clusters, palette = "deep",
+                alpha = 0.9, s = 1,
+                legend = "full")
+
+kmeans2 = KMeans(n_clusters = 5, random_state = 13).fit_predict(review_umapped)
+plt.scatter(tsne2[:, 0], tsne2[:, 1], c = kmeans2, s = 1) 
+plt.show()
+
+kmeans3 = KMeans(n_clusters = 5, random_state = 13).fit_predict(review_encoded)
+plt.scatter(tsne2[:, 0], tsne2[:, 1], c = kmeans3, s = 1) 
+plt.show()
+
+
+
+
+
 
 """
 CLUSTER
-"""
+
 
 cluster = KMeans(n_clusters=SELECTED_K, random_state=22)
 cluster.fit(X)
@@ -171,5 +291,5 @@ for group in groups:
         print(group, file)
         os.rename(file, str(group) + "_" + file)
 
-
+"""
 
--- testingcode.py
+++ testingcode.py
@@ -1,201 +1,101 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-
-import os
-import glob 
-
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as img
-
-import imageio
-import imgaug as ia
-from imgaug import augmenters as iaa
+from PIL import Image
 import cv2
+import glob
 
-from keras.preprocessing import image
-from keras.models import Model, load_model
-from tensorflow.keras.optimizers import Adam
-from keras.callbacks import EarlyStopping, LearningRateScheduler, ModelCheckpoint
-from keras.layers import Input, Dense, Activation, BatchNormalization, Flatten, Conv2D, LeakyReLU
-from keras.layers import MaxPooling2D, Dropout, UpSampling2D
-from keras import regularizers
-import keras.backend as kb
-import zipfile
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+ROOT = "P:/2020/14/Kodning/Scans/all_scans/100signatures/test/others/"
+combined = []
+files = glob.glob(ROOT + "*_sign.jpg")
+tests = glob.glob(ROOT + "*_test.jpg")
+count = 0
+
 
 import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
-
-# Any results you write to the current directory are saved as output.
-
-plt.rcParams['figure.figsize'] = (10.0, 5.0) # set default size of plots
-
-zipfiles = ['train','test','train_cleaned', 'sampleSubmission.csv']
-
-for each_zip in zipfiles:
-    with zipfile.ZipFile("P:/2020/14/Kodning/Code/custodyproject/machine_learning/kaggle/input/denoising-dirty-documents/"+each_zip+".zip","r") as z:
-        z.extractall(".")
-        
-        
+import image_similarity_measures
+from sys import argv
+from image_similarity_measures.quality_metrics import rmse, ssim, sre
 
 
-bsb = img.imread('P:/2020/14/Kodning/Code/custodyproject/machine_learning/kaggle/input/working/train/216.png')
-# test = img.imread('../kaggle/working/test/1.png')
-plt.imshow(bsb, cmap=plt.cm.gray)
+test_img = cv2.imread("P:/2020/14/Kodning/Scans/all_scans/100signatures/test/2062-07_pg1_test.jpg")
 
+ssim_measures = {}
+rmse_measures = {}
+sre_measures = {}
 
+scale_percent = 100 # percent of original img size
+width = int(test_img.shape[1] * scale_percent / 100)
+height = int(test_img.shape[0] * scale_percent / 100)
+dim = (width, height)
+   
+for img_path in files:
+    data_img = cv2.imread(img_path)
+    resized_img = cv2.resize(data_img, dim, interpolation = cv2.INTER_AREA) 
+    ssim_measures[img_path]= ssim(test_img, resized_img) 
+    rmse_measures[img_path]= rmse(test_img, resized_img) 
+    sre_measures[img_path]= sre(test_img, resized_img)
 
-target_width = 540
-target_height = 420
-
-def load_image_from_dir(img_path):
-    file_list = glob.glob(img_path+'/*.png')
-    file_list.sort()
-    img_list = np.empty((len(file_list), target_height, target_width, 1))
-    for i, fig in enumerate(file_list):
-        img = image.load_img(fig, color_mode='grayscale', target_size=(target_height, target_width))
-        img_array = image.img_to_array(img).astype('float32')
-        img_array = img_array / 255.0
-        img_list[i] = img_array
-        print(img_list)
+def calc_closest_val(dict, checkMax):
+    result = {}
+    if (checkMax):
+    	closest = max(dict.values())
+    else:
+    	closest = min(dict.values())
+    		
+    for key, value in dict.items():
+    	print("The difference between ", key ," and the original image is : \n", value)
+    	if (value == closest):
+    	    result[key] = closest
+    	    
+    print("The closest value: ", closest)	    
+    print("######################################################################")
+    return result
     
-    return img_list
+ssim = calc_closest_val(ssim_measures, True)
+rmse = calc_closest_val(rmse_measures, False)
+sre = calc_closest_val(sre_measures, True)
 
-def train_test_split(data,random_seed=55,split=0.75):
-    set_rdm = np.random.RandomState(seed=random_seed)
-    dsize = len(data)
-    ind = set_rdm.choice(dsize,dsize,replace=False)
-    train_ind = ind[:int(0.75*dsize)]
-    val_ind = ind[int(0.75*dsize):]
-    return data[train_ind],data[val_ind]
-
-def augment_pipeline(pipeline, images, seed=5):
-    ia.seed(seed)
-    processed_images = images.copy()
-    for step in pipeline:
-        temp = np.array(step.augment_images(images))
-        processed_images = np.append(processed_images, temp, axis=0)
-    return(processed_images)
-
-
-full_train = load_image_from_dir('P:/2020/14/Kodning/Code/custodyproject/machine_learning/kaggle/input/working/train')
-full_target = load_image_from_dir('P:/2020/14/Kodning/Code/custodyproject/machine_learning/kaggle/input/working/train_cleaned')
-
-
-rotate90 = iaa.Rot90(1) # rotate image 90 degrees
-rotate180 = iaa.Rot90(2) # rotate image 180 degrees
-rotate270 = iaa.Rot90(3) # rotate image 270 degrees
-random_rotate = iaa.Rot90((1,3)) # randomly rotate image from 90,180,270 degrees
-perc_transform = iaa.PerspectiveTransform(scale=(0.02, 0.1)) # Skews and transform images without black bg
-rotate10 = iaa.Affine(rotate=(10)) # rotate image 10 degrees
-rotate10r = iaa.Affine(rotate=(-10)) # rotate image 30 degrees in reverse
-crop = iaa.Crop(px=(5, 32)) # Crop between 5 to 32 pixels
-hflip = iaa.Fliplr(1) # horizontal flips for 100% of images
-vflip = iaa.Flipud(1) # vertical flips for 100% of images
-gblur = iaa.GaussianBlur(sigma=(1, 1.5)) # gaussian blur images with a sigma of 1.0 to 1.5
-motionblur = iaa.MotionBlur(8) # motion blur images with a kernel size 8
-
-seq_rp = iaa.Sequential([
-    iaa.Rot90((1,3)), # randomly rotate image from 90,180,270 degrees
-    iaa.PerspectiveTransform(scale=(0.02, 0.1)) # Skews and transform images without black bg
-])
-
-seq_cfg = iaa.Sequential([
-    iaa.Crop(px=(5, 32)), # crop images from each side by 5 to 32px (randomly chosen)
-    iaa.Fliplr(0.5), # horizontally flip 50% of the images
-    iaa.GaussianBlur(sigma=(0, 1.5)) # blur images with a sigma of 0 to 1.5
-])
-
-seq_fm = iaa.Sequential([
-    iaa.Flipud(1), # vertical flips all the images
-    iaa.MotionBlur(k=6) # motion blur images with a kernel size 6
-])
-
-
-pipeline = []
-pipeline.append(rotate90)
-pipeline.append(rotate180)
-pipeline.append(rotate270)
-pipeline.append(random_rotate)
-pipeline.append(perc_transform)
-pipeline.append(rotate10)
-pipeline.append(rotate10r)
-pipeline.append(crop)
-pipeline.append(hflip)
-pipeline.append(vflip)
-pipeline.append(gblur)
-pipeline.append(motionblur)
-pipeline.append(seq_rp)
-pipeline.append(seq_cfg)
-pipeline.append(seq_fm)
-
-processed_train = augment_pipeline(pipeline, full_train.reshape(-1,target_height,target_width))
-processed_target = augment_pipeline(pipeline, full_target.reshape(-1,target_height,target_width))
-
-processed_train = processed_train.reshape(-1,target_height,target_width,1)
-processed_target = processed_target.reshape(-1,target_height,target_width,1)
-
-print(processed_train.shape)
-
-
-### Multi layer auto encoder with LeakyRelu and Normalization
-input_layer = Input(shape=(None,None,1))
-
-# encoder
-e = Conv2D(32, (3, 3), padding='same')(input_layer)
-e = LeakyReLU(alpha=0.3)(e)
-e = BatchNormalization()(e)
-e = Conv2D(64, (3, 3), padding='same')(e)
-e = LeakyReLU(alpha=0.3)(e)
-e = BatchNormalization()(e)
-e = Conv2D(64, (3, 3), padding='same')(e)
-e = LeakyReLU(alpha=0.3)(e)
-e = MaxPooling2D((2, 2), padding='same')(e)
-
-# decoder
-d = Conv2D(64, (3, 3), padding='same')(e)
-d = LeakyReLU(alpha=0.3)(d)
-d = BatchNormalization()(d)
-
-d = Conv2D(64, (3, 3), padding='same')(d)
-d = LeakyReLU(alpha=0.3)(d)
-# e = BatchNormalization()(e)
-d = UpSampling2D((2, 2))(d)
-d = Conv2D(32, (3, 3), padding='same')(d)
-d = LeakyReLU(alpha=0.2)(d)
-# d = Conv2D(128, (3, 3), padding='same')(d)
-output_layer = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(d)
-
-# optimizer = Adam(lr=1e-4, decay=7e-6)
-optimizer = Adam(lr=9e-4, decay=1e-5)
-AEmodel = Model(input_layer,output_layer)
-AEmodel.compile(loss='mse', optimizer=optimizer)
-AEmodel.summary()
-
-early_stopping = EarlyStopping(monitor='val_loss',
-                               min_delta=0,
-                               patience=10,
-                               verbose=1, 
-                               mode='auto')
-
-checkpoint1 = ModelCheckpoint('best_val_loss.h5',
-                             monitor='val_loss',
-                             save_best_only=True)
-
-checkpoint2 = ModelCheckpoint('best_loss.h5',
-                             monitor='loss',
-                             save_best_only=True)
+print("The most similar according to SSIM: " , ssim)
+print("The most similar according to RMSE: " , rmse)
+print("The most similar according to SRE: " , sre)
 
 
 
-history = AEmodel.fit(processed_train, processed_target,
-                      batch_size=16, epochs=300, verbose=1,callbacks=[early_stopping, checkpoint2])
+"""
+# test image
+for test in tests:
+    print(test)
+    image = cv2.imread(test)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    test_hist = cv2.calcHist([gray_image], [0],
+    						None, [256], [0, 256])
+    
+    # Test img
+    image = cv2.imread(test)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    test_hist = cv2.calcHist([gray_image], [0],None, [256], [0, 256])
 
+# Imgs to compare against test
+for file in files:
+    
+    print(count)
+    count += 1
+    
+    i = 0
+    c = 0
+    
+    image = cv2.imread(file)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    img_hist = cv2.calcHist([gray_image], [0],None, [256], [0, 256])
+    
+    while i<len(test_hist) and i<len(img_hist):
+    	c+=(test_hist[i]-img_hist[i])**2
+    	i+= 1
 
-AEmodel.save('P:/2020/14/Kodning/Code/custodyproject/machine_learning/kaggle/AutoEncoderModelFull.h5')
+    c = c**(1 / 2)
+    combined.append([c, file])
+
+matches = sorted(combined, key=lambda x: x[0], reverse=False)
+
+print(min(matches))
+"""
 
